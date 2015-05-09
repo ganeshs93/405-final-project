f2b25a85a423920d1d3b93b8e6c758394c9a3914
ganeshs93 <gsivakum@usc.edu>
2015-05-08 17:24:49 -0700

    Changed Uber API url

--- config/uber.php
+++ config/uber.php
@@ -2,5 +2,5 @@
 
 return array(
     'server_token' => '0bsBfjDhYWfVcjiLjf_D6Q990NVF-egS_smslF9m',
-    'products_url' => 'https://sandbox-api.uber.com/v1/products'
+    'products_url' => 'https://api.uber.com/v1/products'
 );
